d21e42f6768b421a7d6ec5e970a22a0daf3072f7
zilola <zilolanazarova1994@gmail.com>
2024-01-24 16:46:37 +0500

    Run db:migrate, add sample data to seeds.rb, test CRUD operations on mountains and users tables in rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+admin = User.create(name: 'Tom', role: 'admin');
+
+mountain1 = Mountain.create(
+  name: 'Greater Chimgan',
+  latitude: 41.4951900,
+  longitude: 70.0586718,
+  elevation: 3309.6,
+);
+
+mountain2 = Mountain.create(
+  name: 'Karakush',
+  latitude: 41.2477890,
+  longitude: 70.3391467,
+  elevation: 3864.3,
+);
+
+mountain3 = Mountain.create(
+  name: 'Babaitag',
+  latitude: 41.1549650,
+  longitude: 70.2232660,
+  elevation: 3555.7,
+);
