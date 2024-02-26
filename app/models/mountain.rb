92fe499be1069b594a6c45389fefc4f179f7cfa2
zilola <zilolanazarova1994@gmail.com>
2024-02-26 22:13:40 +0500

    Allow only floats for mountain's longitude and latitude attributes

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,6 +1,6 @@
 class Mountain < ApplicationRecord
   validates :name, presence: true, length: { maximum: 36 }
-  validates :longitude, presence: true, length: { maximum: 36 }
-  validates :latitude, presence: true, length: { maximum: 36 }
+  validates :longitude, presence: true, numericality: { only_float: true }
+  validates :latitude, presence: true, numericality: { only_float: true }
   validates :elevation, presence: true, numericality: { only_float: true, greater_than: 0 }
 end
